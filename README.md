98d4ab48a2cd1d378aff1c152cb0d286dad14639
James Wing Foo Lau <jameslauwf@gmail.com>
2022-11-11 22:01:19 +0000

    Initial working scraper. Output to csv working.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Study: Python program to scrape sites using the BeautifulSoup library
+
+## Steps
+
+In the terminal 
+- pip install beautifulsoup4
+- pip install requests
+- Run the scraper: `python3 scrape.py`
+
+
+scrape.py output sample:
+
+```bash
+
+PS C:\work\code\broex\analysis\scraper-py> python.exe .\scrape.py
+2022-11-11 00:24:35,905 - Extractor - INFO - COMPLETE: Extracted elements from given html page:
+2022-11-11 00:24:35,905 - Extractor - INFO - https://www.spareroom.co.uk/flatshare/?offset=10&search_id=1177415351&sort_by=by_day&mode=list
+
+```
---
+++ extractor_utils.py
@@ -0,0 +1,42 @@
+import logging
+from bs4 import BeautifulSoup
+
+class soup_extractor:
+    '''BS extractor utilities with verification and auditing'''
+
+    # def __init__(self):
+    #     pass
+
+    def extractElementTextValue(self, name, element, obj_, classname): 
+        value = self.extractElementValue(element, obj_, classname, '', True)
+        return value
+
+    def extractElementAttributeValue(self, name, element, obj_, classname, attribute): 
+        return self.extractElementValue(element, obj_, classname, attribute, False)
+
+
+    def extractElementValue(self, element, object_, classname, attribute, text): 
+        value = ''
+        if text == True:
+            value = element.find(object_, class_=classname).text
+            if value is None:
+                logging.warning('element %s has no text value', element)                    
+        else:
+            value = element.find(object_, class_=classname)[attribute]
+            if value is None:
+                logging.warning('element %s has no attribute value', element, attribute)                    
+
+        logging.debug("Value found for element: %s:%s:%s == %s", object_, classname, attribute, value.replace('\n', ''))
+
+        return value
+
+    def verifyValid(self, value):
+        print('Verify if value is valid and perform any cleanup')
+        return value;
+    
+
+    def duplicateCheck(self, key):
+        return key;
+
+    
+    
